ce78d83a122646b06acfbcd10155dd9dc7466b67
shubhendra-20 <37156857+shubhendra-20@users.noreply.github.com>
2019-01-29 18:10:00 +0530

    Add files via upload
    Factorial and Fibonacci Recursion

---
+++ day13/C++/day13_solved.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+
+int fact(int n)
+{
+	if(n == 1)
+  {
+  return 1;
+  }
+	else
+  {
+  return n * fact(n - 1);
+  }
+}
+int fibonacci(int n)
+{
+    if((n==1)||(n==0))
+    {
+        return(n);
+    }
+    else
+    {
+        return(fibonacci(n-1)+fibonacci(n-2));
+    }
+}
+
+int main()
+{
+
+	int n,p,i=0;
+	cin >> n;
+
+	cout << "Factorial= " << fact(n);
+
+  cout<<"enter no. of terms in series";
+  cin>>p;
+  while(i<n)
+    {
+        cout<<" "<<fibonacci(i);
+        i++;
+    }
+
+	return 0;
+}
